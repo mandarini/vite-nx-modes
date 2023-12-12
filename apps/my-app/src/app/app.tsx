f464c60672c1bb2838bb9409cfabb2c72115d1ad
Katerina Skroumpelou <sk.katherine@gmail.com>
2023-12-12 12:29:35 +0200

    react app with different environments

--- apps/my-app/src/app/app.tsx
+++ apps/my-app/src/app/app.tsx
@@ -1,12 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
 
-import NxWelcome from './nx-welcome';
-
 export function App() {
+  const title = import.meta.env.VITE_APP_TITLE;
   return (
     <div>
-      <NxWelcome title="my-app" />
+      <p className={styles.title}>
+        Hello: <strong>{title}</strong>
+      </p>
     </div>
   );
 }
